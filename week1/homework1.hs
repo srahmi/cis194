19bee61db60c3f29eb4b42667c4fca9f239bfe0e
said.rahmi <said.rahmi@argus-presse.fr>
2019-04-23 01:02:28 +0200

    First exercise

--- week1/homework1.hs
+++ week1/homework1.hs
@@ -7,11 +7,20 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev = reverse . toDigits
 
 doubleEveryOhther :: [Integer] -> [Integer]
-doubleEveryOhther [] = []
-doubleEveryOhther (x : []) = [x]
+doubleEveryOhther []             = []
+doubleEveryOhther [x]            = [x]
 doubleEveryOhther (x : (y : zs)) = x : y * 2 : doubleEveryOhther zs
 
 sumDigits :: [Integer] -> Integer
-sumDigits [] = []
-sumDigits [x] = [x]
-sumDigits xs = map toDigits xs
+sumDigits []  = 0
+sumDigits [x] = x
+sumDigits xs  = sum (concatMap toDigits xs)
+
+validate :: Integer -> Bool
+validate =  isValid . sumDigits . doubleEveryOhther . toDigitsRev
+
+isValid :: Integer -> Bool
+isValid n
+  | n <= 0          = False
+  | n `mod` 10 == 0 = True
+  | otherwise       = False
