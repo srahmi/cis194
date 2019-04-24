500c1426f051b3267ce6fb8bb45295f5c3507c53
RAHMI Saïd <said.rahmi@mymoneybank.com>
2019-04-24 11:34:00 +0200

    hanoi tower solution

--- week1/Hanoi.hs
+++ week1/Hanoi.hs
@@ -2,5 +2,8 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi n a b c
-  | n > 0 = Move(a, b) : hanoi n-1 a c b
+hanoi disk source dest spare
+  | disk == 1 = [(source, dest)]
+  | otherwise = hanoi (disk - 1) source spare dest
+      ++ [(source, dest)]
+      ++ hanoi (disk - 1) spare dest source
