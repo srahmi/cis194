73ce74fbb50018627884c4fede5db12d42f07853
said.rahmi <said.rahmi@argus-presse.fr>
2019-05-21 02:09:02 +0200

    add comments to functions

--- week3/homework/Golf.hs
+++ week3/homework/Golf.hs
@@ -6,7 +6,12 @@ import Data.List (group, sort)
 import Control.Monad (join)
 import qualified Data.Map as M
 import Control.Arrow (second)
+import Data.Monoid
 
+{--
+From 0 to xs length return an array of every n-th element in xs
+and filter empty array
+ --}
 skips    :: [a] -> [[a]]
 skips [] = []
 skips xs = filter (not . null) $ map (`every` xs) index
@@ -25,21 +30,39 @@ localMaxima xs = case xs of
     then y : localMaxima (y:z:ys)
     else localMaxima (y:z:ys)
   otherwise -> []
-
+{--
+Lets take this array as example [5,1,1,1]
+            " *        \n *        \n *   *    \n==========\n0123456789\n"
+            appendAxis
+             " *        \n *        \n *   *    \n"
+             . concatMap print'
+             [(3,[1]),(2,[1]),(1,[1,5])]
+             . lines'
+             [(1,1),(1,2),(1,3),(5,1)]
+             . join
+             [[(1,1),(1,2),(1,3)],[(5,1)]]
+             . map (zip [1..])
+             [[1,1,1],[5]]
+             . group
+             [1,1,1,5]
+             . sort
+--}
 histogram :: [Integer] -> String
-histogram = addAxis . print' . line . join . map (zip [1..]) . group . sort
+histogram =  appendAxis
+             . concatMap print' . lines'
+             . join
+             . map (zip [1..])
+             . group . sort
 
-line :: [(Integer, Integer)] -> [(Integer, [Integer])]
-line = reverse
+lines' :: [(Integer, Integer)] -> [(Integer, [Integer])]
+lines' = reverse
        . map (second sort)
        . M.toList
        . M.fromListWith (++)
        . map(\(x, y) -> (x, [y]))
 
-print'              :: [(Integer, [Integer])] -> String
-print' []           = []
-print' ((_, xs):ys) = map(\i -> if i `elem` xs then '*' else ' ') [0..9]
-                         ++ "\n" ++ print' ys
+print'              :: (Integer, [Integer]) -> String
+print' (_, xs) = map(\i -> if i `elem` xs then '*' else ' ') [0..9] ++ "\n"
 
-addAxis :: String -> String
-addAxis s = s ++ "==========\n0123456789\n"
+appendAxis :: String -> String
+appendAxis s = s ++ "==========\n0123456789\n"
