90ffa6a7798ee2a5ca9809b990608bcd8aa93a3f
said.rahmi <said.rahmi@argus-presse.fr>
2019-05-26 22:59:11 +0200

    sieveSundaram and map implementation with foldr

--- week4/homework/Wholemeal.hs
+++ week4/homework/Wholemeal.hs
@@ -1,7 +1,7 @@
 module Wholemeal
     (
     ) where
-
+import Data.List (nub, sort)
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs)
@@ -30,12 +30,31 @@ data Tree a = Leaf
 foldTree :: [a] -> Tree a
 foldTree = foldr insert Leaf
 
+{--This solution doesn't work for arrays of size 3 --}
 insert        :: a -> Tree a -> Tree a
 insert e Leaf = Node 0 Leaf e Leaf
 insert e (Node _ l v r)
-      | height l > height r =  Node (height l + 1) l v (insert e r)
-      | otherwise           =  Node (height r + 1) (insert e l) v r
+      | height l > height r = Node (height l + 1) l v (insert e r)
+      | otherwise           = Node (height r + 1) (insert e l) v r
 
-height      :: Tree a -> Integer
-height Leaf = 0
+height                :: Tree a -> Integer
+height Leaf           = 0
 height (Node h _ _ _) = h
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr ((:) . f) []
+
+xor    :: [Bool] -> Bool
+xor   = foldr (\x acc -> (x && not acc) || (not x && acc)) False
+
+setToRemove   :: Integer -> [Integer]
+setToRemove n = nub
+                . filter (<= n)
+                . map (\(i,j) -> i+j+2*i*j) $ cartProd [1..n-1] [1..n]
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = filter (<= (2 * n + 2)) $ map (\x -> x * 2 + 1)
+                         [x | x <- [1..n],  x `notElem` setToRemove n]
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
