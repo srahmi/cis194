fe8ac37b6142c4ea76b36dac83878a0b2ef405fb
RAHMI <said.rahmi@mymoneybank.com>
2019-06-05 16:35:10 +0200

    1st and 2nd exercices

---
+++ week5/homework/Calculator.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS_GHC -Wall #-}
+module Calculator
+    (eval
+    ) where
+
+import ExprT
+import Parser
+
+eval :: ExprT -> Integer
+eval (Lit x)               = x
+eval (Add (Lit x) (Lit y)) = x + y
+eval (Mul (Lit x) (Lit y)) = x * y
+eval exprT = case exprT of
+  (Add (Lit x) expr)  -> x + eval expr
+  (Add expr (Lit y))  -> eval expr + y
+  (Mul (Lit x) expr)  -> x * eval expr
+  (Mul expr (Lit y))  -> eval expr * y
+  _                   -> 0
+
+
+evalStr :: String -> Maybe Integer
+evalStr = fmap eval . parseExp Lit Add Mul
