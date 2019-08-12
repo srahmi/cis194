a02a0aadcb6aa3345802871b79b586c27e756bb2
said.rahmi <said.rahmi@argus-presse.fr>
2019-08-12 23:51:26 +0200

    Aplicative for Parser fisrt try

--- week9/AParser.hs
+++ week9/AParser.hs
@@ -4,17 +4,17 @@
 module AParser where
 
     import           Control.Applicative()
-    
+
     import           Data.Char
 
     import           Control.Arrow
-    
+
     -- A parser for a value of type a is a function which takes a String
     -- represnting the input to be parsed, and succeeds or fails; if it
     -- succeeds, it returns the parsed value along with the remainder of
     -- the input.
     newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
-    
+
     -- For example, 'satisfy' takes a predicate on Char, and constructs a
     -- parser which succeeds only if it sees a Char that satisfies the
     -- predicate (which it then returns).  If it encounters a Char that
@@ -28,12 +28,12 @@ module AParser where
                             -- of the input (that is, xs)
             | p x       = Just (x, xs)
             | otherwise = Nothing  -- otherwise, fail
-    
+
     -- Using satisfy, we can define the parser 'char c' which expects to
     -- see exactly the character c, and fails otherwise.
     char :: Char -> Parser Char
     char c = satisfy (== c)
-    
+
     {- For example:
     
     *Parser> runParser (satisfy isUpper) "ABC"
@@ -44,7 +44,7 @@ module AParser where
     Just ('x',"yz")
     
     -}
-    
+
     -- For convenience, we've also provided a parser for positive
     -- integers.
     posInt :: Parser Integer
@@ -54,14 +54,23 @@ module AParser where
           | null ns   = Nothing
           | otherwise = Just (read ns, rest)
           where (ns, rest) = span isDigit xs
-    
+
     ------------------------------------------------------------
     -- Your code goes below here
     ------------------------------------------------------------
 
     first' :: (a -> b) -> (a, c) -> (b, c)
-    first' f = mapFst f where
-                 mapFst g (a, b) = (g a, b)
-                 
+    first' f (a, c) = (f a, c)
+
+    -- g = runParser :: String -> Maybe (a, String)
     instance Functor Parser where
-        fmap f (Parser g) = Parser $ fmap (first' f) . g
+      fmap f (Parser g) = Parser (fmap (first' f) . g)
+
+    instance Applicative Parser where
+      pure a    = Parser f where
+                    f s = Just(a, s)
+
+      p1 <*> p2 = 
+
+
+
